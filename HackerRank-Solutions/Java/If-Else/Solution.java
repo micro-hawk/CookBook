4b28c0d7a80234a85ccf6f6633e01b4fe1f7bc6e
micro-hawk <dasvikas0071@gmail.com>
2020-07-28 22:32:18 +0530

    If-Else

---
+++ HackerRank-Solutions/Java/If-Else/Solution.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main ( String[] args ) {
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt ();
+
+        if (n % 2 == 0) {
+            if(n >= 2 && n<=5){
+                System.out.println ("Not Weird");
+            }
+            else if(n>=6 && n<=20){
+                System.out.println ("Weird");
+            }
+            else if(n > 20){
+                System.out.println ("Not Weird");
+            }
+
+        }
+        else{
+            System.out.println ("Weird");
+        }
+    }
+}
