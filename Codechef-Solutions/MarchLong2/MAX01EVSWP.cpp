c3b14546703b1ef60cf70ed8ea2150bb54a223cc
micro-hawk <dasvikas0071@gmail.com>
2022-03-28 19:56:44 +0530

    Some are correct

---
+++ Codechef-Solutions/MarchLong2/MAX01EVSWP.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll main() {
+    ll t; cin >> t; while(t--) {
+        ll len, count=0; string s; cin >> len >> s;
+        for (ll i = 0; i < len; i++)
+        {
+            if(s[i]=='0') count++;
+        } m 
+        
+    }
+}
---
+++ Codechef-Solutions/MarchLong2/THREETOPICS.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+int main() {
+    ll a,b,c,x; cin >> a >> b >> c>> x;
+    if (x==a || x==b || x==c ) cout << "YES\n";
+    else cout << "NO\n";
+}
