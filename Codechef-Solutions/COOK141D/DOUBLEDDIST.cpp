d1637f9c744fe74af439796b1c5f74085581edef
micro-hawk <dasvikas0071@gmail.com>
2022-05-01 22:54:39 +0530

    COOK141D

--- Codechef-Solutions/COOK141D/DOUBLEDDIST.cpp
+++ Codechef-Solutions/COOK141D/DOUBLEDDIST.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
+#include<algorithm>
 #define ll long long
 using namespace std;
 int main() {
 ll t; cin >> t; while(t--) {
         //Write your logic below: 
+        ll size; cin >> size;
+        int * parr = new int[size];
+        for(ll _=0; _<size; _++) cin >> parr[_];
         
+        //printing array.
+        // for(ll _=0; _<size; _++) cout << parr[_] << " ";
+        sort(parr, parr+size);
+        bool fal = true;
+        for (ll k = 1; k < size-1; k++)
+        {
+            ll d1=parr[k] - parr[k-1];
+            ll d2=parr[k+1] - parr[k]; 
+            if(d1 != 2*d2 && 2*d1 != d2) {
+                fal = false;
+                break;
+            }
+        }
+        if (fal) cout << "Yes\n";
+        else cout << "NO\n";
+        delete [] parr;
     }
 }
